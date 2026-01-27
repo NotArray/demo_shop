da54323a297f7263d46efb96eccdba2083978c47
不会数组 <1067780090@qq.com>
2026-01-27 17:20:34 +0800

    主页tab栏实现

--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -1,3 +1,7 @@
+import 'package:demo_shop/pages/Cart/index.dart';
+import 'package:demo_shop/pages/Category/index.dart';
+import 'package:demo_shop/pages/Home/index.dart';
+import 'package:demo_shop/pages/Mine/index.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -8,11 +12,67 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Map<String, String>> _tabList = [
+    {
+      "icon": "lib/assets/ic_public_home_normal.png",
+      "active_icon": "lib/assets/ic_public_home_active.png",
+      "text": "主页",
+    },
+    {
+      "icon": "lib/assets/ic_public_pro_normal.png",
+      "active_icon": "lib/assets/ic_public_pro_active.png",
+      "text": "分类",
+    },
+    {
+      "icon": "lib/assets/ic_public_cart_normal.png",
+      "active_icon": "lib/assets/ic_public_cart_active.png",
+      "text": "购物车",
+    },
+    {
+      "icon": "lib/assets/ic_public_my_normal.png",
+      "active_icon": "lib/assets/ic_public_my_active.png",
+      "text": "我的",
+    },
+  ];
+  int _currentIndex = 0;
+  List<BottomNavigationBarItem> getTabBarWidge() {
+    return List.generate(_tabList.length, (index) {
+      return BottomNavigationBarItem(
+        icon: Image.asset(_tabList[index]["icon"]!, width: 30, height: 30),
+        activeIcon: Image.asset(
+          _tabList[index]["active_icon"]!,
+          width: 30,
+          height: 30,
+        ),
+        label: _tabList[index]["text"],
+      );
+    });
+  }
+
+  List<Widget> _getChildren() {
+    return [HomeView(), CategoryView(), CartView(), MineView()];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("主页"), centerTitle: true),
-      body: Center(child: Text("主页")),
+      //SafeArea避开安全区组件
+      body: SafeArea(
+        child: IndexedStack(children: _getChildren(), index: _currentIndex),
+      ),
+
+      //底部导航栏
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+        unselectedItemColor: Colors.blueGrey,
+        onTap: (int index) {
+          _currentIndex = index;
+          setState(() {});
+        },
+        items: getTabBarWidge(),
+        currentIndex: _currentIndex,
+      ),
     );
   }
 }
